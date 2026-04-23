56a4c4286492514f83d6d81f5e2fde0d9d603d38
backbancher009 <jsjahid20249@gmail.com>
2026-04-23 16:37:40 +0600

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: '𝐙𝐀𝐇𝐈𝐃-𝐁𝐎𝐓',
   author: '‎',
-  botName: "Knight Bot",
+  botName: "𝐙𝐀𝐇𝐈𝐃-𝐁𝐎𝐓",
   botOwner: 'Professor', // Your name
   ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
